df2ed43f35f3932374f2a2271fc2244afb90a3e6
WaittingJun <18363035546@163.com>
2016-06-24 00:12:37 +0800

    TestToString

---
+++ daima/TestToString.java
@@ -0,0 +1,11 @@
+public class TestToString{
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		System.out.println(d);
+	}
+}
+class Dog{
+	//public String toString() {
+	//return "iiii";
+	}
+}
